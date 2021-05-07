f8acb9a95533677de766a5181cdc4f863cd46134
kkeomalaythong <kkeomalaythong117@gmail.com>
2021-05-07 15:07:04 -0400

    Moved chat message code into ChatMessage component

---
+++ pages/ChatMessage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Chat } from './[streamername]';
+
+interface IChatmessageProps {
+	messages: Chat[]
+}
+
+const ChatMessage: React.FunctionComponent<IChatmessageProps> = ({ messages }: IChatmessageProps) => {
+	return (
+		<React.Fragment>
+			{messages.map((x: Chat, i: number) => {
+				const { tags, message, media } = x;
+				const { 'message-type': messageType, 'display-name': displayName, color } = tags;
+
+				if (media) {
+					return <div key={i}><img src={media} /></div>;
+				} else {
+					return <p key={i} className="textstroke"><strong style={{ color: color }}>{displayName}</strong>: {message}</p>;
+				}
+			})}
+		</React.Fragment>
+	);
+};
+
+export default ChatMessage;
